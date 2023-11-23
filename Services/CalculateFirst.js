abd12743c9fc35fa392c229af67db90b702e218e
eveah-codes <52373568+eveah-codes@users.noreply.github.com>
2023-11-23 17:03:07 +0000

    Fixed Calculate.js to fetch recent input values before calculating BMI

--- Services/CalculateFirst.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "./Router.js"
-export function calculateFirst() {
-        const calculateFirst = document.querySelector(".calculate-first")
-        calculateFirst.addEventListener("click", (event) => {
-            event.preventDefault()
-            Router.go("#/calculate")
-        })
-}
